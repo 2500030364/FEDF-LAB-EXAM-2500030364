e3a25da2b6eb3838e399f363426f821564284d03
Tejaswi <2500030364@kluniversity.in>
2026-03-13 20:53:08 +0530

    13th March 2026

--- src/exam/Home.jsx
+++ src/exam/Home.jsx
@@ -6,8 +6,8 @@ export default function Home()
     <div>
         <h2> Home page </h2>
         <p>
-          Employees can register through the Employee Register page.
-          After registering, the employee details will be stored and can be viewed in the Registered Employees section.
+          <pre>Employees can register through the Employee Register page.</pre>
+          <pre>After registering, the employee details will be stored and can be viewed in the Registered Employees section.</pre>
         </p>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYFLXJFFyZEzdg6Pv-F3K1IRn4X_gXaVSkgg&s" alt="Employee" />
       
